515231ed9d900d146b2a88596a6e3e590df73cac
benoit lafaut <benoit.lafaut@gmail.com>
2024-11-13 23:57:30 +0100

    add karaoke

--- CercleRoyalEscrimeTournaisien/GestionClubClass/Paiements_2024_2025.cs
+++ CercleRoyalEscrimeTournaisien/GestionClubClass/Paiements_2024_2025.cs
@@ -15,6 +15,39 @@ namespace WebApplication1.Models
                 {
                      new JourDePrésence()
                      {
+                         DatePrésence = new DateTime(2024,11,13),
+                         EscrimeurId = new List<Guid>()
+                         {
+                             GuidConstantes.GuidMartinSiu,
+                             GuidConstantes.GuidFélixTrannoy,
+                             GuidConstantes.GuidAuroreCarlier,
+                             GuidConstantes.GuidAnaelleIvanov,
+                             GuidConstantes.GuidLiliMestdag,
+                             GuidConstantes.GuidMathildeCarette,
+                             GuidConstantes.GuidAmadoSimon,
+                             //GuidConstantes.GuidEloiBinois,
+                             //GuidConstantes.GuidLouisonBinois,
+                             //GuidConstantes.GuidOscarDeblocq,
+                             //GuidConstantes.GuidArthurBarbery,
+                             //GuidConstantes.GuidJordanMestdagh,
+                             GuidConstantes.GuidRémiSoyez,
+                             //GuidConstantes.GuidRaedwaldVercouter,
+                             //GuidConstantes.GuidMaybelleCarlier,
+                             //GuidConstantes.GuidFabriceRazanajao,
+                             //GuidConstantes.GuidBaptisteMotte,
+                             //GuidConstantes.GuidAbelMotte,
+                             GuidConstantes.GuidApollineOdendhal,
+                             //GuidConstantes.GuidMaeVantroyen,
+                             //GuidConstantes.GuidSachaLessart,
+                             //GuidConstantes.GuidEvaDufrasne,
+                             GuidConstantes.GuidHéloïsePras,
+                             //GuidConstantes.GuidElodieMass,
+                             GuidConstantes.GuidRebeccaVandy,
+                             //GuidConstantes.GuidValentinXXX,
+                         }
+                     },        
+                    new JourDePrésence()
+                     {
                          DatePrésence = new DateTime(2024,11,10),
                          EscrimeurId = new List<Guid>()
                          {
