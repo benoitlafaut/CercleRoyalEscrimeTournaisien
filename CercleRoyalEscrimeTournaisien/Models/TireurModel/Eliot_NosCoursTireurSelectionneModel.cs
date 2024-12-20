49c955c69f7e078b6c35bc0809129d38c1a951d2
benoit lafaut <benoit.lafaut@gmail.com>
2024-12-20 16:58:28 +0100

    eliot

--- CercleRoyalEscrimeTournaisien/Models/TireurModel/Eliot_NosCoursTireurSelectionneModel.cs
+++ CercleRoyalEscrimeTournaisien/Models/TireurModel/Eliot_NosCoursTireurSelectionneModel.cs
@@ -16,31 +16,35 @@ namespace CercleRoyalEscrimeTournaisien
             get
             {
                 List<RemarqueParDate> remarquesParDateTireur = new List<RemarqueParDate>() { };
-                new RemarqueParDate()
+                remarquesParDateTireur.AddRange(new List<RemarqueParDate>()
                 {
-                    DateRemarque = new DateTime(2024, 12, 18),
-                    Arme = TypeArme.Epée,
-                    Tireur = new Tireur()
-                    {
-                        UserNameIndex = this._tireurSelectionne.UserNameIndex,
-                    },
-                    RemarquesData = new List<RemarqueData>()
-                            {
-                             new RemarqueData()
-                                {
-                                    PointNégatif="Ne pense pas toujours à essayer de taper sur le fer adverse mais plutôt à allonger ton bras et pointer ta lame et ton bras vers la cible que tu auras choisie.",
-                                    HasUrlVideo=false,
-                                    Frequency = 1,
-                                },
-                             new RemarqueData()
-                                {
-                                    PointPositif="C'était un beau match face à Martin. Tu menais de 3 0. Et tu as laissé le temps défilé. chapeau !",
-                                    HasUrlVideo=false,
-                                    Frequency = 1,
-                                },
+                     new RemarqueParDate()
+                                    {
+                                        DateRemarque = new DateTime(2024, 12, 18),
+                                        Arme = TypeArme.Epée,
+                                        Tireur = new Tireur()
+                                        {
+                                            UserNameIndex = this._tireurSelectionne.UserNameIndex,
+                                        },
+                                        RemarquesData = new List<RemarqueData>()
+                                                {
+                                                 new RemarqueData()
+                                                    {
+                                                        PointNégatif="Ne pense pas toujours à essayer de taper sur le fer adverse mais plutôt à allonger ton bras et pointer ta lame et ton bras vers la cible que tu auras choisie.",
+                                                        HasUrlVideo=false,
+                                                        Frequency = 1,
+                                                    },
+                                                 new RemarqueData()
+                                                    {
+                                                        PointPositif="C'était un beau match face à Martin. Tu menais de 3 0. Et tu as laissé le temps défilé. chapeau !",
+                                                        HasUrlVideo=false,
+                                                        Frequency = 1,
+                                                    },
 
-                            }
-                };
+                                                }
+                                    }
+                });
+               
 
                 return remarquesParDateTireur;
             }
